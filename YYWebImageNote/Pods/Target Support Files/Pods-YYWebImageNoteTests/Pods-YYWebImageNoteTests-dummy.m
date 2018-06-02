3c43d008dbdad8d7b87d10c32ce97e306a504730
LeeWongSnail <wangli_0632@163.com>
2018-06-02 10:14:20 +0800

    add pod

---
+++ YYWebImageNote/Pods/Target Support Files/Pods-YYWebImageNoteTests/Pods-YYWebImageNoteTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_YYWebImageNoteTests : NSObject
+@end
+@implementation PodsDummy_Pods_YYWebImageNoteTests
+@end
---
+++ YYWebImageNote/Pods/Target Support Files/Pods-YYWebImageNoteUITests/Pods-YYWebImageNoteUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_YYWebImageNoteUITests : NSObject
+@end
+@implementation PodsDummy_Pods_YYWebImageNoteUITests
+@end
